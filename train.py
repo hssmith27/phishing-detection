95065e5981ca37f5e5808dcad53fc79ed72f393e
hssmith27 <hssmith2704@gmail.com>
2025-11-24 18:29:15 -0500

    Added more training data

--- train.py
+++ train.py
@@ -1,39 +1,46 @@
 import pandas as pd
+import glob
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
 
-# Initialize Dataframe
-df = pd.read_csv('data/CEAS_08.csv')
+df_list = []
+data_files = glob.glob('data/*.csv')
 
-# Drop unneeded columns
-df = df.drop(columns=['receiver', 'date'])
+# Combine all data files, taking email body and label
+for data_file in data_files:
+    df_temp = pd.read_csv(data_file)
 
-# Replace null values with empty string
-df['body'] = df['body'].fillna('')
+    if 'text_combined' in df_temp.columns:
+        df_temp['text'] = df_temp['text_combined']
 
-# Remove punctuation and new lines, then strip data
-df['body'] = df['body'].str.replace(r'[^\w\s]', '', regex=True)
-df['body'] = df['body'].str.replace(r'[\r\n]+', ' ', regex=True)
-df['body'] = df['body'].str.strip()
+    elif 'body' in df_temp.columns and 'subject' in df_temp.columns:
+        df_temp['text'] = df_temp['subject'].fillna('') + ' ' + df_temp['body'].fillna('')
+    
+    if 'text' in df_temp.columns and 'label' in df_temp.columns:
+        df_temp = df_temp[['text', 'label']]
+        df_list.append(df_temp)
+
+# Initialize Data
+df = pd.concat(df_list, ignore_index=True)
+
+# Remove new lines, then strip data
+df['text'] = df['text'].str.replace(r'[\r\n]+', ' ', regex=True)
+df['text'] = df['text'].str.strip()
 
 # Get out train/test data
-X = df['body']
+X = df['text']
 Y = df['label']
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=27)
 
 # Feature Extraction
-tfidf = TfidfVectorizer(min_df=1, stop_words='english', lowercase=True)
+tfidf = TfidfVectorizer(ngram_range=(1, 2), stop_words='english', lowercase=True)
 X_train_feat = tfidf.fit_transform(X_train)
 X_test_feat = tfidf.transform(X_test)
 
 # Model Training
 model = LogisticRegression()
 model.fit(X_train_feat, Y_train)
-
-# Test accuracy of model
-Y_pred = model.predict(X_test_feat)
-print(accuracy_score(Y_test, Y_pred))
