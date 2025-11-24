aeb835bf706d1811b7a16b35c5f6efd373f3fa6e
hssmith27 <hssmith2704@gmail.com>
2025-11-23 23:11:53 -0500

    Updated README and implemented model training

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ It includes a front end where users can submit email content and receive the pro
 
 **Front End:** React & Typescript
 **Back End:** Python & Flask
+
+## How to Use:
+
+1. Download Kaggle data by running `download_data.py`
+**Note:** Running `download_data.py` requires a Kaggle API key to authenticate.
+
+## Credits:
+- *Al-Subaiey, A., Al-Thani, M., Alam, N. A., Antora, K. F., Khandakar, A., & Zaman, S. A. U. (2024, May 19) Novel Interpretable and Robust Web-based AI Platform for Phishing Email Detection. ArXiv.org. https://arxiv.org/abs/2405.11619*
--- train.py
+++ train.py
@@ -1,7 +1,39 @@
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
+# Initialize Dataframe
 df = pd.read_csv('data/CEAS_08.csv')
 
-print(df)
+# Drop unneeded columns
+df = df.drop(columns=['receiver', 'date'])
+
+# Replace null values with empty string
+df['body'] = df['body'].fillna('')
+
+# Remove punctuation and new lines, then strip data
+df['body'] = df['body'].str.replace(r'[^\w\s]', '', regex=True)
+df['body'] = df['body'].str.replace(r'[\r\n]+', ' ', regex=True)
+df['body'] = df['body'].str.strip()
+
+# Get out train/test data
+X = df['body']
+Y = df['label']
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=27)
+
+# Feature Extraction
+tfidf = TfidfVectorizer(min_df=1, stop_words='english', lowercase=True)
+X_train_feat = tfidf.fit_transform(X_train)
+X_test_feat = tfidf.transform(X_test)
+
+# Model Training
+model = LogisticRegression()
+model.fit(X_train_feat, Y_train)
+
+# Test accuracy of model
+Y_pred = model.predict(X_test_feat)
+print(accuracy_score(Y_test, Y_pred))
